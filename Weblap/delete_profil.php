415c88e15e03ec1eb3444366977a291fcb5b0c2d
Levente Janos Rez <ice3886@gmail.com>
2022-10-06 14:19:55 +0200

    fix delete profil

--- Weblap/delete_profil.php
+++ Weblap/delete_profil.php
@@ -1,11 +1,9 @@
 <?php
     session_start();
     require 'mydbms.php';
-    $con = mysqli_connect('localhost', 'root', '', 'beadando_feladat', 3306);
+    $con = mysqli_connect('localhost', 'root', '', 'mini_projekt_1', 3306);
 
-    $profilkep = $_POST['profilkep'];
     $nev = $_POST['nev'];
-    $eletkor = $_POST['eletkor'];
     $email = $_POST['email'];
     $jelszo = md5($_POST['jelszo']);
     $id = $_SESSION['id'];

fe458ae73025304534d5a5a1e729c8e5b321178b
Levente Janos Rez <ice3886@gmail.com>
2022-10-06 14:44:03 +0200

    Add gamemod desc.

---
+++ Weblap/gamemode_select.php
@@ -0,0 +1,7 @@
+<?php 
+/*
+    Ide egy tábla kellene vagy 1 oszloppal és N sorral vagy forditva.
+    Benne lennének gombok, amik átdobnak a kiválasztott játékmód .php
+    oldalára, ha úgy csináljuk.
+*/
+?>

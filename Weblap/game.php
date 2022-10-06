98b73c9ff2bd04fdfaf31c48bda429d6b99b1467
Levente Janos Rez <ice3886@gmail.com>
2022-10-06 14:29:17 +0200

    Create game page

---
+++ Weblap/game.php
@@ -0,0 +1,5 @@
+<?php 
+/*Ezen az oldalon lesz maga a játék lebonyolitva, el lehet dönteni,
+hogy a különböző játékmódokat külön-külön lapon implementáljuk,
+vagy mind kapnak egy .php oldalt. (Pl.: 1v1.php, 3v3.php, 5v5.php stb.) */
+?>

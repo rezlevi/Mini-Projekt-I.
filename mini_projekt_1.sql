be0ec0bdb9fea8810419bb504ba50cf12f177c3c
StangaPeter <113850216+StangaPeter@users.noreply.github.com>
2022-10-07 17:03:49 +0200

    Adatbázis bővítése statisztika táblával

--- mini_projekt_1.sql
+++ mini_projekt_1.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2022. Okt 04. 16:50
--- Kiszolgáló verziója: 10.4.22-MariaDB
--- PHP verzió: 8.1.2
+-- Létrehozás ideje: 2022. Okt 07. 16:21
+-- Kiszolgáló verziója: 10.4.24-MariaDB
+-- PHP verzió: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,6 +20,22 @@ SET time_zone = "+00:00";
 --
 -- Adatbázis: `mini_projekt_1`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Tábla szerkezet ehhez a táblához `statistics`
+--
+
+CREATE TABLE `statistics` (
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `all` int(11) NOT NULL,
+  `won` int(11) NOT NULL,
+  `lost` int(11) NOT NULL,
+  `draw` int(11) NOT NULL,
+  `percentage` double NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 -- --------------------------------------------------------
 
@@ -47,6 +63,13 @@ INSERT INTO `users` (`id`, `username`, `email`, `jelszo`, `admin`) VALUES
 --
 
 --
+-- A tábla indexei `statistics`
+--
+ALTER TABLE `statistics`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `FK` (`user_id`);
+
+--
 -- A tábla indexei `users`
 --
 ALTER TABLE `users`
@@ -57,10 +80,26 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT a táblához `statistics`
+--
+ALTER TABLE `statistics`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT a táblához `users`
 --
 ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Megkötések a kiírt táblákhoz
+--
+
+--
+-- Megkötések a táblához `statistics`
+--
+ALTER TABLE `statistics`
+  ADD CONSTRAINT `FK` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
